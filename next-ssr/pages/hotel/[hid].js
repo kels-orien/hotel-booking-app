848aa8732ac0db367014d2833ccd9a11771ad524
Kohwo Orien <kowo4real@yahoo.co.uk>
2020-08-13 20:55:50 +0100

    Add dynamic hotel page

---
+++ next-ssr/pages/hotel/[hid].js
@@ -0,0 +1,33 @@
+
+
+const Hotel  = ({hotel}) => {
+  
+  return <p>Hotel Info: {hotel}</p>
+}
+
+
+
+// This function gets called at build time
+export async function getStaticPaths() {
+    // Call an external API endpoint to get hotels
+    const res = await fetch('https://.../hotels')
+    const hotels = await res.json()
+  
+    // Get the paths we want to pre-render based on hotels
+    const paths = hotels.map((hotel) => `/hotel/${hotel.id}`)
+  
+    // We'll pre-render only these paths at build time.
+    // { fallback: false } means other routes should 404.
+    return { paths, fallback: false }
+  }
+
+  export async function getStaticProps({ params }) {
+    // params contains the hotel `id`.
+    // If the route is like /hotel/1, then params.id is 1
+    const res = await fetch(`https://.../hotel/${params.id}`)
+    const hotel = await res.json()
+  
+    // Pass hotel data to the page via props
+    return { props: { hotel } }
+  }
+export default Hotel
