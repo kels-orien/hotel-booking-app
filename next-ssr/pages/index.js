3fa6ff7c51e62d58ad0276e08db79bca6d9d8178
Kohwo Orien <kowo4real@yahoo.co.uk>
2020-08-19 21:58:04 +0100

    update casing for components path

--- next-ssr/pages/index.js
+++ next-ssr/pages/index.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
-import Nav from "../components/Nav";
-import Footer from "../components/Footer";
+import Nav from "../components/nav";
+import Footer from "../components/footer";
 import Search from "../components/search";
 import Slider from "../components/slider";
-import Thumbnail from "../components/Thumbnail";
+import Thumbnail from "../components/thumbnail";
 import Header from "../components/header";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import PageBody from "../components/pagebody";
 
 export default function Home() {
