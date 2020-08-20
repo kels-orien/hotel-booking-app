d861700408df78499471444831ac4122834d8b40
Kohwo Orien <kowo4real@yahoo.co.uk>
2020-08-20 17:30:23 +0100

    Update index with  props

--- next-ssr/pages/index.js
+++ next-ssr/pages/index.js
@@ -8,20 +8,37 @@ import Header from "../components/header";
 import Layout from "../components/layout";
 import PageBody from "../components/pagebody";
 
-export default function Home() {
+export default function Home({data}) {
   return (
     <div className="container">
-      <Layout>
+     <Layout>
         <Header>
           <Nav />
         </Header>
         <PageBody>
           <Search />
           <Slider />
-          <Thumbnail />
+          {data.map((thumb) => (
+            <Thumbnail
+              key={thumb._id}
+              name={thumb.name}
+              city={thumb.city}
+              thumb_url={thumb.thumb_url}
+            />
+          ))}
         </PageBody>
         <Footer />
       </Layout>
     </div>
   );
+}
+
+
+export async function getServerSideProps() {
+  // params contains the thumb `id`.
+  // If the route is like /thumb/1, then params.id is 1
+  const res = await fetch(`VERCEL_URL` + `/api/hotels`);
+
+  const data = await res.json();
+  return { props: { data } };
 }
