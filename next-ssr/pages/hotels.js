24005dfbc0865ec58911949dbe1090875c4808c2
Kohwo Orien <kowo4real@yahoo.co.uk>
2020-08-20 13:29:45 +0100

    Change props to "getServerSideProps"

--- next-ssr/pages/hotels.js
+++ next-ssr/pages/hotels.js
@@ -33,11 +33,11 @@ export default function Hotels({ hotels }) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   // params contains the hotel `id`.
   // If the route is like /hotel/1, then params.id is 1
   const res = await fetch(`VERCEL_URL` + `/api/hotels`);
-  //console.log(res.json());
+
   const hotels = await res.json(); // Pass hotel data to the page via props
   return { props: { hotels } };
 }
