d6cfac299311fcba57b04be1e583c4d79a37a2ee
Kohwo Orien <kowo4real@yahoo.co.uk>
2020-08-21 00:15:19 +0100

    prop change

--- next-ssr/pages/index.js
+++ next-ssr/pages/index.js
@@ -7,19 +7,25 @@ import Thumbnail from "../components/thumbnail";
 import Header from "../components/header";
 import Layout from "../components/layout";
 import PageBody from "../components/pagebody";
-import Hotel from "../components/hotel";
-export default function Home({ data }) {
+
+export default function Home({data}) {
   return (
     <div className="container">
-      <Layout>
+     <Layout>
         <Header>
           <Nav />
         </Header>
         <PageBody>
           <Search />
           <Slider />
-
-          {data ? <Header /> : <Hotel />}
+          {data.map((thumb) => (
+            <Thumbnail
+              key={thumb._id}
+              name={thumb.name}
+              city={thumb.city}
+              thumb_url={thumb.thumb_url}
+            />
+          ))}
         </PageBody>
         <Footer />
       </Layout>
@@ -27,9 +33,12 @@ export default function Home({ data }) {
   );
 }
 
-export async function getStaticProps() {
-  
-  const res = await fetch(`https://hotel-booking-app.vercel.app/api/hotels`);
+
+export async function getServerSideProps() {
+  // params contains the thumb `id`.
+  // If the route is like /thumb/1, then params.id is 1
+  const res = await fetch(`VERCEL_URL` + `/api/hotels`);
+
   const data = await res.json();
   return { props: { data } };
 }
