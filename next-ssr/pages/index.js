e02e1f169dd8543a90e09ce36e9acb517c42565d
Kohwo Orien <kowo4real@yahoo.co.uk>
2020-08-20 23:01:03 +0100

    url update

--- next-ssr/pages/index.js
+++ next-ssr/pages/index.js
@@ -30,7 +30,8 @@ export default function Home({ data }) {
 export async function getStaticProps() {
   // params contains the thumb `id`.
   // If the route is like /thumb/1, then params.id is 1
-  const res = await fetch(`VERCEL_URL` + `/api/hotels`);
+  const res = await fetch(`https://hotel-booking-app.vercel.app
+  /api/hotels`);
 
   const data = await res.json();
   return { props: { data } };
