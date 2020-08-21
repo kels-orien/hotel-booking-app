d55802c3f87201d25135bbbefe28dff14ae43fc7
Kohwo Orien <kowo4real@yahoo.co.uk>
2020-08-21 19:20:47 +0100

    edit url and staticprops

--- next-ssr/pages/homepage.js
+++ next-ssr/pages/homepage.js
@@ -1,12 +1,43 @@
+import Head from "next/head";
+import Nav from "../components/nav";
+import Footer from "../components/footer";
+import Search from "../components/search";
+import Slider from "../components/slider";
+import Thumbnail from "../components/thumbnail";
+import Header from "../components/header";
+import Layout from "../components/layout";
+import PageBody from "../components/pagebody";
 
-
-function homepage() {
+function homepage({data}) {
     return (
-        <div className="b-color">
-            <h1>Hello World!</h1>
-        </div>
+        <Layout>
+        <Header>
+          <Nav />
+        </Header>
+        <PageBody>
+          <Search />
+          <Slider />
+          {data.map((thumb) => (
+            <Thumbnail
+              key={thumb._id}
+              name={thumb.name}
+              city={thumb.city}
+              thumb_url={thumb.thumb_url}
+            />
+          ))}
+        </PageBody>
+        <Footer />
+      </Layout>
     )
 }
 
+export async function getStaticProps() {
+    // params contains the thumb `id`.
+    // If the route is like /thumb/1, then params.id is 1
+    const res = await fetch(`VERCEL_URL` + `/api/hotels`);
+  
+    const data = await res.json();
+    return { props: { data } };
+  }
 
 export default homepage
