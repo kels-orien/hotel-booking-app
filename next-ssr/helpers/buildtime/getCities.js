642add08f1e8162342cca02a837076f2d953f680
Kohwo Orien <kowo4real@yahoo.co.uk>
2020-09-19 14:04:43 +0100

    Change route url to vercel url

--- next-ssr/helpers/buildtime/getCities.js
+++ next-ssr/helpers/buildtime/getCities.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async () => {
-  const res = await axios.get(`VERCEL_URL` + `/api/city`);
+  const res = await axios.get(process.env.VERCEL_URL + `/api/city`);
 
   return res.data;
 };
