87a842684f947e203cfc383bedc16069e3b1fd20
Kohwo Orien <kowo4real@yahoo.co.uk>
2020-09-19 14:58:18 +0100

    Edit route url

--- next-ssr/helpers/buildtime/getTopDeals.js
+++ next-ssr/helpers/buildtime/getTopDeals.js
@@ -1,10 +1,9 @@
 import axios from "axios";
-
-
 const VERCEL_URL = process.env.VERCEL_URL;
 
+
 export default async () => {
-  const res = await axios.get(`https://` + VERCEL_URL + `/api/top`);
+  const res = await axios.get(`https://${VERCEL_URL}/api/top`);
 
   return res.data;
 };
